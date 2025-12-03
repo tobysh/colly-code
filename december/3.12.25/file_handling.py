770b9f1e402a87b01df898f2475ad226822382a1
Toby <2185810@student.wiltshire.ac.uk>
2025-12-03 12:24:08 +0000

    added file handling files

--- december/3.12.25/file_handling.py
+++ december/3.12.25/file_handling.py
@@ -1,3 +1,9 @@
 with open("etc/names.dat", "r") as file:
+    print(file.readline())
+
+with open("etc/names.dat", "r") as file:
+    print(file.readlines())
+
+with open("etc/names.dat", "r") as file:
     print(file.read())
 
