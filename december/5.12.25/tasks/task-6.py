f90c1ad95ef41d547459be9cd1ac4c36b26e1c79
Toby <2185810@student.wiltshire.ac.uk>
2025-12-05 14:08:30 +0000

    completed task 6

--- december/5.12.25/tasks/task-6.py
+++ december/5.12.25/tasks/task-6.py
@@ -4,4 +4,9 @@ file_path = get_path("assets", "foods.txt")
 print("Enter three of your favourite foods:")
 with open(file_path, "w") as file:
     for i in range(3):
-        file.write(input(f"({i}) >"))
+        file.write(f"{input(f"({i+1}) >")}\n")
+
+with open(file_path, "r") as file:
+    print("Your favourite foods are:")
+    for line in file.readlines():
+        print(line.strip())
