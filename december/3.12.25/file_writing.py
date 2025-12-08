61b64d5246f9b89dbb95db039ec47bbf58946fc6
tobysh <2185810@student.wiltshire.ac.uk>
2025-12-08 10:37:39 +0000

    updated file_writing to use an etc folder

--- december/3.12.25/file_writing.py
+++ december/3.12.25/file_writing.py
@@ -1,11 +1,11 @@
 st1Name = input("Enter Name: ")
 t1Mark = input(f"Enter mark for {st1Name}: ")
-with open("marks.txt", "w") as f:
+with open("etc/marks.txt", "w") as f:
     f.write("Alice,85\n")
     f.write("Bob,74\n")
-    f.write(f"{st1Name}, {t1Mark}\n")
+    f.write(f"{st1Name},{t1Mark}\n")
     
-with open("marks.txt", "r") as f:
+with open("etc/marks.txt", "r") as f:
     for line in f:
         name, mark = line.strip().split(",") # saving in 2 lists
         print(name, "scored ",mark)
