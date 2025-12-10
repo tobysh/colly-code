2176e69641d81669c6f1a5821c8330b3802a3bf8
Toby <2185810@student.wiltshire.ac.uk>
2025-12-10 15:43:10 +0000

    Commit script, default message

---
+++ december/10.12.25/tasks/task-4.py
@@ -0,0 +1,10 @@
+from modules.get_file_path import get_path
+
+
+while True:
+    query = input("Enter a word to input:").split(" ")
+    if len(query) > 1:
+        print("Only enter one word")
+
+
+
