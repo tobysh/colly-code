f7bf52a6cf4606a2a51b0dd3c8b84a326b95802e
Toby <2185810@student.wiltshire.ac.uk>
2025-12-10 15:50:50 +0000

    Commit script, default message

--- december/10.12.25/tasks/task-4.py
+++ december/10.12.25/tasks/task-4.py
@@ -1,11 +1,20 @@
 from modules.get_file_path import get_path
 
+file_path = get_path(file_name="story.txt")
 
 while True:
     query = input("Enter a word to input: ").split(" ")
     if len(query) > 1:
         print("Only enter one word")
     else:
+        query = (query[0])
         break
 
+with open(file_path, "r") as file:
+    words = file.read().split(" ")
+    count = 0
+    for word in words:
+        if word == query:
+            count+=1
 
+print(f"The word {query} appears {count} times in the file.")
