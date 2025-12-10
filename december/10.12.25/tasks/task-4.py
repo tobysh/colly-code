2f5eb43455a87fb5874a21d31fb0372fe2189ec7
Toby <2185810@student.wiltshire.ac.uk>
2025-12-10 15:43:27 +0000

    Commit script, default message

--- december/10.12.25/tasks/task-4.py
+++ december/10.12.25/tasks/task-4.py
@@ -2,9 +2,10 @@ from modules.get_file_path import get_path
 
 
 while True:
-    query = input("Enter a word to input:").split(" ")
+    query = input("Enter a word to input: ").split(" ")
     if len(query) > 1:
         print("Only enter one word")
-
+    else:
+        break
 
 
