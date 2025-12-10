6fe688ad08ec8e1033b5ed1572066d88ed93216f
Toby <2185810@student.wiltshire.ac.uk>
2025-12-10 15:37:45 +0000

    Commit script, default message

--- december/10.12.25/tasks/task-1.py
+++ december/10.12.25/tasks/task-1.py
@@ -2,7 +2,7 @@ from modules.get_file_path import get_path
 
 file_path = get_path("assets","profile.txt")
 
-name = input("What\'s your name?\n>")
+name = input("What\'s your name?\n>").strip().capitalize()
 
 age = None
 while not age:
@@ -13,4 +13,4 @@ while not age:
         print("Enter a valid number.")
 
 with open(file_path, "w") as file:
-    file.write(f"My name is {name} and I\'m {age} years olds")
+    file.write(f"My name is {name} and I\'m {age} years old\n")
--- december/10.12.25/tasks/task-2.py
+++ december/10.12.25/tasks/task-2.py
@@ -1,3 +1,6 @@
 from modules.get_file_path import get_path
 
-p
+file_path = get_path("assets", "profile.txt")
+
+with open(file_path, "r") as file:
+    print(file.read().upper())
