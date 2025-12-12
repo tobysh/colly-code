80075e7ee5aff28d1147f8e80aacbf6606aad1fc
Toby <2185810@student.wiltshire.ac.uk>
2025-12-12 14:38:14 +0000

    ye

--- december/12.12.25/database_admore.py
+++ december/12.12.25/database_admore.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-sample_df = pd.read_csv(r"C:\Users\2185810\Documents\college\colly-code\december\12.12.25\pandas_data.csv")
+sample_df = pd.read_csv(r"C:\Users\2185810\Documents\college\colly-code\december\12.12.25\sales.txt",sep="$")
 print(sample_df.shape)
