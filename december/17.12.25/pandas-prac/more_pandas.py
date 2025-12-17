d668ce4f7eea481d8d6eeb18eab0b7aeb00b01ef
Toby <2185810@student.wiltshire.ac.uk>
2025-12-17 15:48:42 +0000

    corrected code (i get to go home now)

--- december/17.12.25/pandas-prac/more_pandas.py
+++ december/17.12.25/pandas-prac/more_pandas.py
@@ -11,10 +11,10 @@ print(df.describe()) #1e
 
 df_calories = df['Calories'] #1f
 df_duration_calories = df[['Duration', 'Calories']] #1g
-df_mid = df.iloc[[4,17]] #1i
+df_mid = df.iloc[4:19] #1i
 df_1j = df.iloc[9][["Duration", "Pulse"]] #1j
-df_1k = df.iloc[[10,20]] #1k
-df_1L = df.iloc[[9,19]][["Pulse","Calories"]] #1l
+df_1k = df.iloc[10:21] #1k
+df_1L = df.iloc[9:20, [1,3]] #1l
 print(f"💠---Task 1f---\n{df_calories}")
 print(f"💠---Task 1g---\n{df_duration_calories}")
 print(f"💠---Task 1h---\n{df.dtypes}") #1h
@@ -22,6 +22,6 @@ print(f"💠---Task 1i---\n{df_mid}")
 print(f"💠---Task 1j---\n{df_1j}")
 print(f"💠---Task 1k---\n{df_1k}")
 print(f"💠---Task 1l---\n{df_1L}")
-
+print(df_1j.info())
 
 #Task 2, The difference between df.iloc[10:20] and df.loc[10:20] is that iloc begins indexing at 0 while loc begins indexing at 1, meaning iloc has 1 earlier index.
