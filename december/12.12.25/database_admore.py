0530be75e01095ecf82e220d5a3e49b945127ed8
Toby <2185810@student.wiltshire.ac.uk>
2026-01-05 13:34:33 +0000

    rafwgg

--- december/12.12.25/database_admore.py
+++ december/12.12.25/database_admore.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-sample_df = pd.read_csv(r"C:\Users\2185810\Documents\college\colly-code\december\12.12.25\sales.txt",sep="$")
-print(sample_df.shape)
+supermarket = pd.read_csv(r"supermarket.csv")
+pharmacy = pd.read_json(r"pharmacy.json")
+print(f"Supermarket: {supermarket.head(12)}")
+print(f"Pharmacy: {pharmacy.head(12)}")
+
+print(f"Supermarket shape: {supermarket.shape}")
+print(f"Pharmacy shape: {pharmacy.shape}")
+
+print(f"Supermarket Info: {supermarket.info()}")
+print(f"Pharmacy Info: {pharmacy.info()}")
+
+print(f"Supermarket Description: {supermarket.describe()}")
+print(f"Pharmacy Description: {pharmacy.describe()}")
+
+outputs = f"---Supermarket---\n{supermarket.head(12)}\n---Pharmacy---\n{pharmacy.head(12)}\n---Supermarket Shape---\n{supermarket.shape}\n---Pharmacy Shape---\n{pharmacy.shape}\n---Supermarket Info---\n{supermarket.info()}\n---Pharmacy Info---\n{pharmacy.info()}\n---Supermarket Description---\n{supermarket.describe()}\n---Pharmacy Description---\n{pharmacy.describe()}"
+with open("outputs.txt", "w") as file:
+    file.write(outputs)
