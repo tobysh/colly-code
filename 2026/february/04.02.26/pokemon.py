41e718d618de54fa67d0215083cfa6a891d061b4
Toby <2185810@student.wiltshire.ac.uk>
2026-02-04 16:00:01 +0000

    hello

--- 2026/february/04.02.26/pokemon.py
+++ 2026/february/04.02.26/pokemon.py
@@ -27,3 +27,23 @@ print(speedyPokemon)
 
 #TASK 4
 print(f"---Task 4---\n{df.iloc[0:10,0:5]}")
+
+#TASK 5
+df["Power_index"] = df["Attack"]+df["Defense"]+df["Speed"]
+print(f"---Task 5.1---")
+print(df.head())
+df["is_strong"]=df["Power_index"]>250
+print(f"---Task 5.2---")
+print(df.head())
+
+#TASK 6
+print(f"---Task 6.1---")
+sorted_speed = df.sort_values("Speed", ascending=False)
+print(sorted_speed.head())
+print(f"---Task 6.2---")
+sorted_hp = df.sort_values("HP")
+print(sorted_hp.head())
+sorted_defense = df.sort_values("Defense")
+print(sorted_defense.head())
+sorted_total = df.sort_values("Total", ascending=False)
+print(f"---Task 6.3---\n{sorted_total.head(10)}")
