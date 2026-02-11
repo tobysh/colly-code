691ff462c107782009761eab1fdea47c3a9fba50
Toby <2185810@student.wiltshire.ac.uk>
2026-02-11 16:07:50 +0000

    afwafa

--- 2026/february/11.02.26/show.py
+++ 2026/february/11.02.26/show.py
@@ -4,17 +4,24 @@ from modules.get_file_path import get_path
 df= pd.read_csv(get_path(file_name="11.THardwareSales.csv"))
 df['DateTime'] = pd.to_datetime(df['DateTime'])
 df['Total_Sales'] = pd.to_numeric(df['Total_Sales'])
-print(df.info())
 
-
-total_sales = (df.iloc[0:20]).groupby('Region')['Total_Sales'].sum()
-region = total_sales.index
-print(region)
-plt.pie(total_sales, labels=list(region))
-
-#plt.plot(x,y, color="red",marker="*",linestyle="dashdot")
-plt.title('Sales over time Graph')
+plt.hist(df['Total_Sales'], bins=10, color='purple')
+plt.title('Distribution of Total Sales')
 plt.xlabel('Total Sales')
-plt.ylabel('Date')
-plt.legend()
+plt.ylabel('Frequency')
+plt.show()
+
+plt.hist(df['Units_Sold'], bins=5, color='lime')
+plt.title('Distribution of Units Sold')
+plt.xlabel('Units Sold')
+plt.ylabel('Frequency')
+plt.show()
+
+traffic = pd.read_json(get_path(file_name="traffic_data.json"))
+vehicle_count = (df.iloc[0:100]).groupby('location')['vehicle_count'].sum()
+location = vehicle_count.index
+plt.bar(location, vehicle_count, color='lime')
+plt.title('Traffic by location')
+plt.xlabel('Location')
+plt.ylabel('Vehicle count')
 plt.show()
