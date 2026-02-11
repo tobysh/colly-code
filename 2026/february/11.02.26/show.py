747ee5fd5c4dcbc5a2326138448d5b55332090ce
Toby <2185810@student.wiltshire.ac.uk>
2026-02-11 15:53:22 +0000

    hello

--- 2026/february/11.02.26/show.py
+++ 2026/february/11.02.26/show.py
@@ -3,10 +3,18 @@ import pandas as pd
 from modules.get_file_path import get_path
 df= pd.read_csv(get_path(file_name="11.THardwareSales.csv"))
 df['DateTime'] = pd.to_datetime(df['DateTime'])
-x= df['DateTime']
-y = df['Total_Sales']
-plt.plot(x,y)
+df['Total_Sales'] = pd.to_numeric(df['Total_Sales'])
+print(df.info())
+
+
+total_sales = (df.iloc[0:20]).groupby('Region')['Total_Sales'].sum()
+region = total_sales.index
+print(region)
+plt.pie(total_sales, labels=list(region))
+
+#plt.plot(x,y, color="red",marker="*",linestyle="dashdot")
 plt.title('Sales over time Graph')
 plt.xlabel('Total Sales')
 plt.ylabel('Date')
+plt.legend()
 plt.show()
